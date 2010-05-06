d42763ec5a6d991b743a6496f64faf8624d3a4b2
Patrick Hall <pat@amundo.com>
2010-05-06 13:56:03 -0700

    factored out header & footer

---
+++ inc/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>elsie</title>
+  <meta charset="utf-8">
+  <link type="text/css" href="css/style.css" rel="stylesheet" />
+  <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+  <!--[if lt IE 9]><script src="http://ie7-js.googlecode.com/svn/version/2.1%28beta3%29/IE9.js" type="text/javascript"></script><![endif]-->
+  <!--[if lt IE 7]><link rel="stylesheet" type="text/css" media="all" href="/javascripts/ie6.css"/><![endif]-->
+
+<script src="js/jquery.js"> </script>
+<script src="js/jquery.annotatevideo.js"> </script>
+<script src="js/translit.js"> </script>
+<script src="js/elsie.js"> </script>
+
+
+</head>
+<body>
+
